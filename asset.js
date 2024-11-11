47c1561a8a57a655452aba1e6f8f43c357c8a9f5
Kmore1515 <kylebradleymorris@usf.edu>
2024-11-10 23:02:24 -0500

    Task 1: Create asset module with asset list and getAssetId function.

---
+++ asset.js
@@ -0,0 +1,18 @@
+export const assetArray = [
+    { id: 1, name: "Tesla", type: "stock", price: 320, quantity: 25
+    },
+
+    { id: 2, name: "Apple", type: "bond", price: 95, quantity: 50
+    }
+]
+
+export function getAssetbyId(assetId) {
+    const returnAsset = assetArray.find(asset => assetId === asset.id)
+
+    if (assetWanted) {
+        return returnAsset
+    }   else {
+        return null
+    }
+}
+
